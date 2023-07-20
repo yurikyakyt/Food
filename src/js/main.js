b627ce4132048420680fb9509bbeedc4c0612966
Yurikyakyt <dzhatiev80@mail.ru>
2023-07-20 13:37:49 +0300

    Add tabs script

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const tabs = document.querySelectorAll(".tabheader__item"),
+          tabcontent = document.querySelectorAll(".tabcontent"),
+          tabsparent = document.querySelector(".tabheader__items");
+    
+    function hideTabContent() {
+        tabcontent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabcontent[i].style.display = 'block';
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsparent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains("tabheader__item")){
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);   
+                }
+            });
+        }
+
+    });
+});
