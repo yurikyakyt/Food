2a55b696d362b043b07d247bf14439a323eea68b
Yurikyakyt <dzhatiev80@mail.ru>
2023-07-26 10:59:36 +0300

    Add slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,7 +6,12 @@ var __webpack_exports__ = {};
 window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabcontent = document.querySelectorAll(".tabcontent"),
-    tabsparent = document.querySelector(".tabheader__items");
+    tabsparent = document.querySelector(".tabheader__items"),
+    offer_slides = document.querySelectorAll(".offer__slide"),
+    offer_current_slide = document.querySelector("#current"),
+    offer_slide_next_btn = document.querySelector(".offer__slider-next"),
+    offer_slide_prev_btn = document.querySelector(".offer__slider-prev");
+  console.log(offer_current_slide);
   function hideTabContent() {
     tabcontent.forEach(item => {
       item.style.display = 'none';
@@ -30,6 +35,40 @@ window.addEventListener("DOMContentLoaded", () => {
           showTabContent(i);
         }
       });
+    }
+  });
+  function hideSliderContent() {
+    offer_slides.forEach(slide => {
+      slide.style.display = 'none';
+    });
+  }
+  function showSliderContent(i = 0) {
+    offer_slides[i].style.display = 'block';
+  }
+  hideSliderContent();
+  showSliderContent();
+  offer_slide_next_btn.addEventListener('click', e => {
+    let curNum = +offer_current_slide.textContent - 1;
+    if (curNum == 3) {
+      offer_current_slide.textContent = "01";
+      hideSliderContent();
+      showSliderContent(0);
+    } else {
+      hideSliderContent();
+      offer_current_slide.textContent = `0${curNum + 2}`;
+      showSliderContent(curNum + 1);
+    }
+  });
+  offer_slide_prev_btn.addEventListener('click', e => {
+    let curNum = +offer_current_slide.textContent - 1;
+    if (curNum == 0) {
+      offer_current_slide.textContent = "04";
+      hideSliderContent();
+      showSliderContent(3);
+    } else {
+      hideSliderContent();
+      offer_current_slide.textContent = `0${curNum - 2}`;
+      showSliderContent(curNum - 1);
     }
   });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const tabs = document.querySelectorAll(".tabheader__item"),
           tabcontent = document.querySelectorAll(".tabcontent"),
-          tabsparent = document.querySelector(".tabheader__items");
+          tabsparent = document.querySelector(".tabheader__items"),
+          offer_slides = document.querySelectorAll(".offer__slide"),
+          offer_current_slide = document.querySelector("#current"),
+          offer_slide_next_btn = document.querySelector(".offer__slider-next"),
+          offer_slide_prev_btn = document.querySelector(".offer__slider-prev");
+          console.log(offer_current_slide);
+
     
     function hideTabContent() {
         tabcontent.forEach(item => {
@@ -35,4 +41,45 @@ window.addEventListener("DOMContentLoaded", () => {
         }
 
     });
+
+
+
+    function hideSliderContent() {
+        offer_slides.forEach(slide => {
+            slide.style.display = 'none';
+        })
+    }
+
+    function showSliderContent(i = 0) {
+        offer_slides[i].style.display = 'block';
+    }
+
+    hideSliderContent();
+    showSliderContent();
+
+    offer_slide_next_btn.addEventListener('click', (e) => {
+        let curNum = +(offer_current_slide.textContent) - 1;
+        if(curNum == 3){
+            offer_current_slide.textContent = "01";
+            hideSliderContent();
+            showSliderContent(0);
+        }else{
+            hideSliderContent();
+            offer_current_slide.textContent = `0${curNum+2}`;
+            showSliderContent(curNum+1);
+        }
+    })
+
+    offer_slide_prev_btn.addEventListener('click', (e) => {
+        let curNum = +(offer_current_slide.textContent) - 1;
+        if(curNum == 0){
+            offer_current_slide.textContent = "04";
+            hideSliderContent();
+            showSliderContent(3);
+        }else{
+            hideSliderContent();
+            offer_current_slide.textContent = `0${curNum-2}`;
+            showSliderContent(curNum-1);
+        }
+    })
 });
